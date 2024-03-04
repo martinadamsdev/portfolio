b75f8059b875ec1a9b23812a01d142b1fad3ae52
martinadamsdev <martinageradams@gmail.com>
2024-03-04 09:15:02 +0800

    feat: add the @next/bundle-analyzer.

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
+import withBundleAnalyzer from '@next/bundle-analyzer'
+
+withBundleAnalyzer({
+    enabled: process.env.ANALYZE === 'true',
+})
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default nextConfig;
+export default nextConfig
