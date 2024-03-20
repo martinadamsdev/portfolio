75b1b12fdf82264796ccde753452d7bc0e615810
martinadamsdev <martinageradams@gmail.com>
2024-03-21 02:53:00 +0800

    feat: add Google Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Analytics } from '@vercel/analytics/react';
 import { SpeedInsights } from '@vercel/speed-insights/next';
+import { GoogleAnalytics } from '@next/third-parties/google'
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -19,9 +20,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-      {children}
-      <Analytics />
-      <SpeedInsights />
+        {children}
+        <Analytics />
+        <SpeedInsights />
+        <GoogleAnalytics gaId="G-LBTZ0QHWKS" />
       </body>
     </html>
   );
