36182f2d092092adc09dca02391f377e0abde6ec
martinadamsdev <martinageradams@gmail.com>
2024-03-21 09:57:24 +0800

    feat: add Google Analytics and remove @vercel/analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import { Analytics } from '@vercel/analytics/react';
 import { SpeedInsights } from '@vercel/speed-insights/next';
 import { GoogleAnalytics } from '@next/third-parties/google'
 import "./globals.css";
+import React from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,9 +21,8 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         {children}
-        <Analytics />
-        <SpeedInsights />
         <GoogleAnalytics gaId="G-LBTZ0QHWKS" />
+        <SpeedInsights />
       </body>
     </html>
   );
